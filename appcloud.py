7d09060929049ce61cc9df41ffb9466d9dbefe9a
Ruchinzz <ruchinaudichya09@gmail.com>
2025-05-24 20:01:53 +0530

    Initial commit for BakchodAI v1.0

--- appcloud.py
+++ appcloud.py
@@ -5,9 +5,7 @@ from datetime import datetime
 import re
 import os
 import urllib.parse
-from dotenv import load_dotenv
 
-load_dotenv()
 OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 
 st.set_page_config(page_title="BakchodAI – Meme Startup Generator", page_icon="🧠", layout="centered")
